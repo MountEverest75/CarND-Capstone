e1286c66207d61f093fc166fc7674a8e181fc30c
MountEverest <ravikiranmailid@gmail.com>
2018-01-18 11:05:43 -0800

    Chore: Waypoint updater full, twist controller and dbw_node test versions

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -10,31 +10,26 @@ from twist_controller import Controller
 
 '''
 You can build this node only after you have built (or partially built) the `waypoint_updater` node.
-
 You will subscribe to `/twist_cmd` message which provides the proposed linear and angular velocities.
 You can subscribe to any other message that you find important or refer to the document for list
 of messages subscribed to by the reference implementation of this node.
-
 One thing to keep in mind while building this node and the `twist_controller` class is the status
 of `dbw_enabled`. While in the simulator, its enabled all the time, in the real car, that will
 not be the case. This may cause your PID controller to accumulate error because the car could
 temporarily be driven by a human instead of your controller.
-
 We have provided two launch files with this node. Vehicle specific values (like vehicle_mass,
 wheel_base) etc should not be altered in these files.
-
 We have also provided some reference implementations for PID controller and other utility classes.
 You are free to use them or build your own.
-
 Once you have the proposed throttle, brake, and steer values, publish it on the various publishers
 that we have created in the `__init__` function.
-
 '''
 
 class DBWNode(object):
     def __init__(self):
         rospy.init_node('dbw_node')
 
+        # Extract all parameters from ros server
         vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
         fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
         brake_deadband = rospy.get_param('~brake_deadband', .1)
@@ -46,6 +41,19 @@ class DBWNode(object):
         max_lat_accel = rospy.get_param('~max_lat_accel', 3.)
         max_steer_angle = rospy.get_param('~max_steer_angle', 8.)
 
+        # Make a dictionary to pass the parameters on to the controller
+        Parameters = {'vehicle_mass': vehicle_mass,
+                      'fuel_capacity': fuel_capacity,
+                      'brake_deadband': brake_deadband,
+                      'decel_limit': decel_limit,
+                      'accel_limit': accel_limit,
+                      'wheel_radius': wheel_radius,
+                      'wheel_base': wheel_base,
+                      'steer_ratio': steer_ratio,
+                      'max_lat_accel': max_lat_accel,
+                      'max_steer_angle': max_steer_angle}
+
+        # Publishers
         self.steer_pub = rospy.Publisher('/vehicle/steering_cmd',
                                          SteeringCmd, queue_size=1)
         self.throttle_pub = rospy.Publisher('/vehicle/throttle_cmd',
@@ -53,28 +61,95 @@ class DBWNode(object):
         self.brake_pub = rospy.Publisher('/vehicle/brake_cmd',
                                          BrakeCmd, queue_size=1)
 
-        # TODO: Create `TwistController` object
-        # self.controller = TwistController(<Arguments you wish to provide>)
+        self.controller = Controller(Parameters)
 
-        # TODO: Subscribe to all the topics you need to
+        self.dbw_enabled = False
+        self.current_velocity = None
+        self.twist_cmd = None
+
+        # Subscribers
+        rospy.Subscriber('/current_velocity', TwistStamped,
+                         self.current_velocity_cb, queue_size=1)
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool,
+                         self.dbw_enabled_cb, queue_size=1)
+        rospy.Subscriber('/twist_cmd', TwistStamped,
+                         self.twist_cmd_cb, queue_size=5)
 
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50) # 50Hz
+        """loop"""
+        rate = rospy.Rate(50)  # 50Hz
         while not rospy.is_shutdown():
-            # TODO: Get predicted throttle, brake, and steering using `twist_controller`
-            # You should only publish the control commands if dbw is enabled
-            # throttle, brake, steering = self.controller.control(<proposed linear velocity>,
-            #                                                     <proposed angular velocity>,
-            #                                                     <current linear velocity>,
-            #                                                     <dbw status>,
-            #                                                     <any other argument you need>)
-            # if <dbw is enabled>:
-            #   self.publish(throttle, brake, steer)
+            throttle, brake, steering = self.controller.contrl(arg1,
+                                                               arg2,
+                                                               arg3,
+                                                               arg4,
+                                                               arg5)
+
+            if self.dbw_enabled:
+                self.publish(throttle, brake, steering)
+
             rate.sleep()
 
+    def current_velocity_cb(self, msg):
+        """current_velocity_cb
+        Callback function for current_velocity
+        :param msg:
+            [geometry_msgs/TwistStamped]:
+            std_msgs/Header header
+              uint32 seq
+              time stamp
+              string frame_id
+            geometry_msgs/Twist twist
+              geometry_msgs/Vector3 linear
+                float64 x
+                float64 y
+                float64 z
+              geometry_msgs/Vector3 angular
+                float64 x
+                float64 y
+                float64 z
+        """
+        self.current_velocity = msg
+
+    def dbw_enabled_cb(self, msg):
+        """dbw_enabled_cb
+        Callback function for dbw_enabled
+        :param msg:
+            [std_msgs/Bool]:
+            bool data
+        """
+        self.dbw_enabled = msg
+
+    def twist_cmd_cb(self, msg):
+        """twist_cmd_cb
+        Callback function for twist_cmd
+        :param msg:
+            [geometry_msgs/TwistStamped]:
+            std_msgs/Header header
+              uint32 seq
+              time stamp
+              string frame_id
+            geometry_msgs/Twist twist
+              geometry_msgs/Vector3 linear
+                float64 x
+                float64 y
+                float64 z
+              geometry_msgs/Vector3 angular
+                float64 x
+                float64 y
+                float64 z
+        """
+        self.twist_cmd = msg
+
     def publish(self, throttle, brake, steer):
+        """publish
+        Publish the control values to the cmd nodes
+        :param throttle:
+        :param brake:
+        :param steer:
+        """
         tcmd = ThrottleCmd()
         tcmd.enable = True
         tcmd.pedal_cmd_type = ThrottleCmd.CMD_PERCENT
