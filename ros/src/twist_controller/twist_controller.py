76b5c55a9c7891ed1b8d98e2b0384c27da2913b9
MountEverest <ravikiranmailid@gmail.com>
2018-01-19 10:13:34 -0800

    Chore: Integrated Twist Controller code

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,53 +1,72 @@
-import rospy
+import math
 from pid import PID
-from lowpass import LowPassFilter
 from yaw_controller import YawController
+from lowpass import LowPassFilter
 
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
+MIN_SPEED = 0
 
 
-class TwistController(object):
-    def __init__(self, cp):
-        self.yaw_controller = YawController(
-            wheel_base=cp.wheel_base,
-            steer_ratio=cp.steer_ratio,
-            min_speed=cp.min_speed,
-            max_lat_accel=cp.max_lat_accel,
-            max_steer_angle=cp.max_steer_angle)
+class Controller(object):
+    """Controller"""
+    def __init__(self, **parameters):
 
-        self.cp = cp
-        self.pid = PID(kp=5, ki=0.5, kd=0.5, mn=cp.decel_limit, mx=cp.accel_limit)
-        self.s_lpf = LowPassFilter(tau = 3, ts = 1)
+        self.parameters = parameters
+
+        self.speed_pid = PID(2.0, 0.02, 0.02,
+                             parameters['decel_limit'],
+                             parameters['accel_limit'])
+
+        # self.steer_pid = PID(0.5, 0.0, 0.02,
+        #                      -parameters['max_steer_angle'],
+        #                      parameters['max_steer_angle'])
+
+        self.yaw_control = YawController(parameters['wheel_base'],
+                                         parameters['steer_ratio'],
+                                         MIN_SPEED,
+                                         parameters['max_lat_accel'],
+                                         parameters['max_steer_angle'])
+
+        # TODO Tune the PID and see if we need to use filter
         self.t_lpf = LowPassFilter(tau = 3, ts = 1)
+        self.s_lpf = LowPassFilter(tau = 3, ts = 1)
 
-    def reset(self):
-        self.pid.reset()
+    def control(self, twist_cmd, current_velocity, time_interval):
+        """control
+        :param twist_cmd:
+        :param current_velocity:
+        :param time_interval:
+        """
+        # Calculate throttle
+        target_velocity = twist_cmd.twist.linear.x
+        real_velocity = current_velocity.twist.linear.x
+        vel_err = target_velocity - real_velocity
 
-    def control(self, twist_cmd, current_velocity, del_time):
+        throttle = self.speed_pid.step(vel_err, time_interval)
+        throttle = self.t_lpf.filt(throttle)
 
-        lin_vel = abs(twist_cmd.twist.linear.x)
-        ang_vel = twist_cmd.twist.angular.z
-        vel_err = lin_vel - current_velocity.twist.linear.x
+        # TODO Need to modify to use CTE
+        # Calculate steering
+        angular_velocity = twist_cmd.twist.angular.z
+        target_steering = self.yaw_control.get_steering(target_velocity,
+                                                        angular_velocity,
+                                                        real_velocity)
+        target_steering = self.s_lpf.filt(target_steering)
 
-        next_steer = self.yaw_controller.get_steering(lin_vel, ang_vel, current_velocity.twist.linear.x)
-        next_steer = self.s_lpf.filt(next_steer)
-
-        acceleration = self.pid.step(vel_err, del_time)
-        acceleration = self.t_lpf.filt(acceleration)
-
-        if acceleration > 0.0:
-            throttle = acceleration
+        # Calculate brake
+        if throttle > 0.0:
             brake = 0.0
         else:
+            decel = abs(throttle)
+            if decel < self.parameters['brake_deadband']:
+                decel = 0.0
+            brake = decel * (self.parameters['vehicle_mass'] + self.parameters['fuel_capacity'] * GAS_DENSITY) * self.parameters['wheel_radius']
+
             throttle = 0.0
-            deceleration = -acceleration
 
-            if deceleration < self.cp.brake_deadband:
-                deceleration = 0.0
+        return throttle, brake, target_steering
 
-            brake = deceleration * (self.cp.vehicle_mass + self.cp.fuel_capacity * GAS_DENSITY) * self.cp.wheel_radius
-
-        # Return throttle, brake, steer
-        return throttle, brake, next_steer
+    def reset(self):
+        self.speed_pid.reset()
